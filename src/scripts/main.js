724e055ca16f59bd290d387175193718d19186b8
pedroDev <playstarpedro@gmail.com>
2024-02-29 14:02:15 -0300

    criando a contagem regressiva

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,33 @@
-console.log('olá mundo')
+AOS.init();
+
+const dataDaEvento = new Date("Dec 12, 2024 19:00:00");
+const timeStampDoEvento = dataDaEvento.getTime();
+
+const contaAsHoras = setInterval(function() {
+    const agora = new Date();
+    const timeStampAtual = agora.getTime()
+
+    const distanciaAteOEvento = timeStampDoEvento - timeStampAtual;
+
+    const diaEmMs = 1000 * 60 * 60 * 24;
+    const horaEmMs = 1000 * 60 * 60;
+    minutoEmMs = 1000 * 60;
+
+    const diasAteOEvento = Math.floor(distanciaAteOEvento / diaEmMs) 
+    const horasAteOEvento = Math.floor((distanciaAteOEvento % diaEmMs) / horaEmMs)
+    const minutosAteOEvento = Math.floor((distanciaAteOEvento % horaEmMs) /  minutoEmMs)
+    const segundosAteoEvento = Math.floor((distanciaAteOEvento % minutoEmMs) / 1000);
+
+
+    console.log(diasAteOEvento);
+    console.log(horasAteOEvento);
+    console.log(minutosAteOEvento);
+    console.log(segundosAteoEvento);
+
+    document.getElementById('contador').innerHTML = `${diasAteOEvento}d ${horasAteOEvento}h ${minutosAteOEvento}m ${segundosAteoEvento}s`
+
+    if(distanciaAteOEvento < 0) {
+        clearInterval(contaAsHoras);
+        document.getElementById('contador').innerHTML = 'Evento expirado'
+    }
+}, 1000)
